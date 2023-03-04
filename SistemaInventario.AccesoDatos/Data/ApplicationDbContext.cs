d2849dcfb379242c1fca4128bfc4aa51513507b0
Carlos Piedra <barce90@hotmail.com>
2023-03-04 14:53:10 -0800

    Creacion Proyecto

---
+++ SistemaInventario.AccesoDatos/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SistemaInventario.Modelos;
+
+namespace SistemaInventario.AccesoDatos.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Bodega> Bodegas { get; set; }
+
+    }
+}
---
+++ SistemaInventario.Modelos/ErrorViewModels/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaInventario.Modelos.ErrorViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
